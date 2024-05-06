e1ec2feff836d7a16278cecd807debea8647c388
babacark <babacar.k@itdm-group.com>
2024-05-06 18:50:00 +0100

    ajout utilisateur dans avis

--- src/main/java/com/example/avis/demo/entites/Avis.java
+++ src/main/java/com/example/avis/demo/entites/Avis.java
@@ -1,9 +1,6 @@
 package com.example.avis.demo.entites;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,5 +15,7 @@ public class Avis {
     private Long id;
     private String message;
     private String statut;
+    @ManyToOne
+    private Utilisateur utilisateur;
 
 }
--- src/main/java/com/example/avis/demo/web/AvisRestController.java
+++ src/main/java/com/example/avis/demo/web/AvisRestController.java
@@ -2,12 +2,15 @@ package com.example.avis.demo.web;
 
 
 import com.example.avis.demo.entites.Avis;
+import com.example.avis.demo.entites.Utilisateur;
 import com.example.avis.demo.services.AvisService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -18,6 +21,8 @@ public class AvisRestController {
 
     @PostMapping("/avis")
     public void creer(@RequestBody Avis avis){
+        Utilisateur utilisateur = (Utilisateur) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        avis.setUtilisateur(utilisateur);
         this.avisService.creerAvis(avis);
 
     }
